dc45253db32769f73c37e437e633154fdf8cbc97
Maxwell <m.kurbatov@g.nsu.ru>
2024-02-10 20:18:18 +0700

    added tests

---
+++ ast_analyzer/tests/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/maxwell/LuNA-Static-Analysis-Repository/ast_analyzer/tests/test_serialize.cpp" "CMakeFiles/main.dir/test_serialize.cpp.o" "gcc" "CMakeFiles/main.dir/test_serialize.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- ast_analyzer/tests/test_analyzer/ucodes.cpp
+++ ast_analyzer/tests/test_analyzer/ucodes.cpp
@@ -3,9 +3,9 @@
 
 extern "C" {
 
-void c_init(int val, OutputDF &df) {
-	df.setValue(val);
-	printf("c_init: %d, size: %d\n", val, (int)df.getSize());
+void c_init(OutputDF &df) {
+	df.setValue(1);
+	// printf("c_init: %d, size: %d\n", val, (int)df.getSize());
 }
 
 void c_iprint(int df) {
@@ -15,6 +15,5 @@ void c_iprint(int df) {
 void c_print(InputDF& df) {
 	printf("c_print %d\n", df.getValue<int>());
 }
-
+void c_foo(int) {}
 }
-
