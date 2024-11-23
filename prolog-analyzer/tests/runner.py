358e27f486481e3ec017759e7b8329300385e3ad
mmichurov <mikhail.michurov.00@gmail.com>
2024-11-24 05:57:17 +0700

    Escape `"` in commands

--- prolog-analyzer/tests/runner.py
+++ prolog-analyzer/tests/runner.py
@@ -201,12 +201,14 @@ def run_test(test_name: str, test_dir: Path) -> bool:
         if not command.available:
             continue
 
-        printer.print(f'{prefix}Running "{command.command}"')
+        command_str = command.command.replace('"', '\\"')
+        printer.print(f'{prefix}Running "{command_str}"')
         _, error = command.execute(test_dir)
         if error:
             return fail(error)
 
-    printer.print(f'{prefix}Running "{act_command.command}"')
+    command_str = act_command.command.replace('"', '\\"')
+    printer.print(f'{prefix}Running "{command_str}"')
     process, error = act_command.execute(test_dir)
     if error:
         return fail(error)
