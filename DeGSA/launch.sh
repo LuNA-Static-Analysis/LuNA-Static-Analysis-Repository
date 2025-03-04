03912743ceff52b0a2764cec3fc5eb9bf24ee670
v.tsarev <v.tsarev@g.nsu.ru>
2025-03-04 18:00:57 +0700

    unnecessary commit before merge

--- DeGSA/launch.sh
+++ DeGSA/launch.sh
@@ -4,12 +4,12 @@ rm debug_launch_degsa_output.txt
 bison -d ../parser/grammar.ypp
 flex ../parser/lexics.l
 
-python3 ../parser/pp.py luna/tests/syn/syn8.fa -o prepr.fa
+python3 ../parser/pp.py luna/launchable/source.fa -o prepr.fa
 g++ lex.yy.c -c -o lex.yy.o
 g++ grammar.tab.cpp -c -o grammar.tab.o
 g++ -std=gnu++0x src/main.cpp -O2 -c -o degsa.o
 g++ Exprtk/exprtk.o lex.yy.o grammar.tab.o degsa.o -o degsa.out
-./degsa.out prepr.fa luna/tests/syn/syn8.fa
+./degsa.out prepr.fa luna/launchable/source.fa
 
 rm grammar.tab.cpp
 rm grammar.tab.hpp
